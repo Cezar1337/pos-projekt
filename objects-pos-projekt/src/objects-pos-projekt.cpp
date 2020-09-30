5bc1be6829d9ed55f43cad82ccf62001b3bf4ec2
Cezary Kiedrowski <cezary.kiedrowski@gmail.com>
2020-09-30 09:35:19 +0100

    initial hello world commit

---
+++ objects-pos-projekt/src/objects-pos-projekt.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : objects-pos-projekt.cpp
+// Author      : Cezary Kiedrowski
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
