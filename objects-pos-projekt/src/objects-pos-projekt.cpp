c9a44f16e6f324f33bcfb9dbde47922f3bcb541a
Arek <arektuex@wp.pl>
2020-09-30 16:10:52 +0200

    noewe nowewe

--- objects-pos-projekt/src/objects-pos-projekt.cpp
+++ objects-pos-projekt/src/objects-pos-projekt.cpp
@@ -1,3 +1,5 @@
+
+
 //============================================================================
 // Name        : objects-pos-projekt.cpp
 // Author      : Cezary Kiedrowski, Arkadiusz Dumin
@@ -11,9 +13,28 @@
 #include <pthread.h>
 using namespace std;
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	cout<<"plz pull this from origin why doesnt it work god damn it"<<endl; //my patience is being tested
-	cout<<"plz pull this from origin why doesnt it work god damn it"<<endl;
-	return 0;
+#define NUM_THREADS 5
+
+void *PrintHello(void *threadid) {
+   long tid;
+   tid = (long)threadid;
+   cout << "Hello World! Thread ID, " << tid << endl;
+   pthread_exit(NULL);
+}
+
+int main () {
+   pthread_t threads[NUM_THREADS];
+   int rc;
+   int i;
+
+   for( i = 0; i < NUM_THREADS; i++ ) {
+      cout << "main() : creating thread, " << i << endl;
+      rc = pthread_create(&threads[i], NULL, PrintHello, (void *)i);
+
+      if (rc) {
+         cout << "Error:unable to create thread," << rc << endl;
+         exit(-1);
+      }
+   }
+   pthread_exit(NULL);
 }
