9428850857f4f1d3498b22e23e35b1c9314ade25
Cezary Kiedrowski <cezary.kiedrowski@gmail.com>
2020-10-17 12:26:36 +0100

    create empty listener-pos-projekt project inside existing repo

---
+++ listener-pos-projekt/src/listener-pos-projekt.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : listener-pos-projekt.cpp
+// Author      : Cezary Kiedrowski
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
